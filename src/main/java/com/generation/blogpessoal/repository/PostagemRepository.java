3481a7eebce8f860cb8bbd3df46ffb5fffe3b202
Natalia <natalia.s36.ns@gmail.com>
2022-02-26 20:28:33 -0300

    atualizando

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,10 +3,14 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.PostagemModel;
-
+@Repository
 public interface PostagemRepository extends JpaRepository<PostagemModel, Long> {
 	List<PostagemModel> findAllByTituloContainingIgnoreCase(String titulo);
 	//select * from tb_postagens where titulo like %titulo%;
+	//JpaReposotory precisa de 2 parametros, a tabela que você quer manipular, e o seu id
+
+	
 	}
